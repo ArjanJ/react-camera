da705448649dec88e41343da6ceb56045c609dd9
Arjan <arjan.jassal@poweredbygrow.com>
2018-01-03 17:30:04 -0800

    move polyfill into new file

--- src/camera/Camera.js
+++ src/camera/Camera.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import debounce from 'throttle-debounce/debounce';
+import './polyfill';
 import CameraError from './CameraError';
 import CaptureButton from './CaptureButton';
 import CameraWrapper from './CameraWrapper';
@@ -9,34 +10,6 @@ import SwitchModeButton from './SwitchModeButton';
 import { errorTypes } from './errorTypes';
 import { facingModes } from './facingModeTypes';
 import { buildConstraints, getAvailableDevices } from './cameraUtils';
-
-if (navigator.mediaDevices === undefined) {
-  navigator.mediaDevices = {};
-}
-
-// Some browsers partially implement mediaDevices. We can't just assign an object
-// with getUserMedia as it would overwrite existing properties.
-// Here, we will just add the getUserMedia property if it's missing.
-if (navigator.mediaDevices.getUserMedia === undefined) {
-  navigator.mediaDevices.getUserMedia = function(constraints) {
-    // First get ahold of the legacy getUserMedia, if present
-    var getUserMedia =
-      navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-    // Some browsers just don't implement it - return a rejected promise with an error
-    // to keep a consistent interface
-    if (!getUserMedia) {
-      return Promise.reject(
-        new Error('getUserMedia is not implemented in this browser'),
-      );
-    }
-
-    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
-    return new Promise(function(resolve, reject) {
-      getUserMedia.call(navigator, constraints, resolve, reject);
-    });
-  };
-}
 
 class Camera extends PureComponent {
   constructor(props) {
